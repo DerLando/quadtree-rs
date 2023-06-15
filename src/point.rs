35e8cb1ab329a76dbccffa03244ebd0040dd79f9
Lando <lando.schumpich@gmail.com>
2023-06-15 13:44:54 +0200

    Document the point struct

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,7 @@
+/// A simple 2-dimensional point struct.
+/// Implemented here to don't have any external dependencies.
+/// In the future there should probably a feature to replace this
+/// With a widely used point type from the rust eco system...
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Point {
     x: f32,
@@ -5,18 +9,21 @@ pub struct Point {
 }
 
 impl Point {
+    /// Create a new [`Point`] from it's x and y components
     pub const fn new(x: f32, y: f32) -> Self {
         Self { x, y }
     }
 
+    /// Read back the inner x component of the [`Point`]
     pub fn x(&self) -> f32 {
         self.x
     }
+    /// Read back the inner y component of the [`Point`]
     pub fn y(&self) -> f32 {
         self.y
     }
 
-    pub fn squared_distance(&self, other: &Point) -> f32 {
+    pub(crate) fn squared_distance(&self, other: &Point) -> f32 {
         (self.x - other.x).powi(2) + (self.y - other.y).powi(2)
     }
 }
